7b313d80c169a8b4a9573900c219f8443d57bbb8
ashdik <ashdik.saxophone@gmail.com>
2021-10-18 01:10:32 +0900

    Restruct all tests with 'group' like describe/context/it

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,65 +3,77 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:readable_test/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+  group('[Increment Button]', () {
+    group('label shows "0"', () {
+      testWidgets('label shows "1"', (WidgetTester tester) async {
+        await tester.pumpWidget(const MyApp());
 
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+        expect(find.text('0'), findsOneWidget);
+        expect(find.text('1'), findsNothing);
 
-    await tester.tap(find.byKey(incrementKey));
-    await tester.pump();
+        await tester.tap(find.byKey(incrementKey));
+        await tester.pump();
 
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+        expect(find.text('0'), findsNothing);
+        expect(find.text('1'), findsOneWidget);
+      });
+    });
+
+    group('label shows "1"', () {
+      testWidgets('label shows "2"', (WidgetTester tester) async {
+        await tester.pumpWidget(const MyApp());
+
+        await tester.tap(find.byKey(incrementKey));
+        await tester.pump();
+
+        expect(find.text('1'), findsOneWidget);
+        expect(find.text('2'), findsNothing);
+
+        await tester.tap(find.byKey(incrementKey));
+        await tester.pump();
+
+        expect(find.text('1'), findsNothing);
+        expect(find.text('2'), findsOneWidget);
+      });
+    });
   });
 
-  testWidgets('Counter increments smoke test 2', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+  group('[Decrement Button]', () {
+    group('label shows "1"', () {
+      testWidgets('label shows "0"', (WidgetTester tester) async {
+        await tester.pumpWidget(const MyApp());
 
-    await tester.tap(find.byKey(incrementKey));
-    await tester.pump();
+        await tester.tap(find.byKey(incrementKey));
+        await tester.pump();
 
-    expect(find.text('1'), findsOneWidget);
-    expect(find.text('2'), findsNothing);
+        expect(find.text('1'), findsOneWidget);
+        expect(find.text('0'), findsNothing);
 
-    await tester.tap(find.byKey(incrementKey));
-    await tester.pump();
+        await tester.tap(find.byKey(decrementKey));
+        await tester.pump();
 
-    expect(find.text('1'), findsNothing);
-    expect(find.text('2'), findsOneWidget);
-  });
+        expect(find.text('0'), findsOneWidget);
+        expect(find.text('1'), findsNothing);
+      });
+    });
 
-  testWidgets('Counter decrements smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    group('label shows "2"', () {
+      testWidgets('label shows "1"', (WidgetTester tester) async {
+        await tester.pumpWidget(const MyApp());
 
-    await tester.tap(find.byKey(incrementKey));
-    await tester.pump();
+        await tester.tap(find.byKey(incrementKey));
+        await tester.tap(find.byKey(incrementKey));
+        await tester.pump();
 
-    expect(find.text('1'), findsOneWidget);
-    expect(find.text('0'), findsNothing);
+        expect(find.text('2'), findsOneWidget);
+        expect(find.text('1'), findsNothing);
 
-    await tester.tap(find.byKey(decrementKey));
-    await tester.pump();
+        await tester.tap(find.byKey(decrementKey));
+        await tester.pump();
 
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-  });
-
-  testWidgets('Counter decrements smoke test 2', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-
-    await tester.tap(find.byKey(incrementKey));
-    await tester.tap(find.byKey(incrementKey));
-    await tester.pump();
-
-    expect(find.text('2'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    await tester.tap(find.byKey(decrementKey));
-    await tester.pump();
-
-    expect(find.text('2'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+        expect(find.text('2'), findsNothing);
+        expect(find.text('1'), findsOneWidget);
+      });
+    });
   });
 }
