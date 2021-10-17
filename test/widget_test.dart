94a433bbaeb890a2b94e1452e325151f802d8cbc
ashdik <ashdik.saxophone@gmail.com>
2021-10-17 12:09:53 +0900

    Add some tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,67 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:readable_test/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byKey(incrementKey));
     await tester.pump();
 
-    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+
+  testWidgets('Counter increments smoke test 2', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.byKey(incrementKey));
+    await tester.pump();
+
+    expect(find.text('1'), findsOneWidget);
+    expect(find.text('2'), findsNothing);
+
+    await tester.tap(find.byKey(incrementKey));
+    await tester.pump();
+
+    expect(find.text('1'), findsNothing);
+    expect(find.text('2'), findsOneWidget);
+  });
+
+  testWidgets('Counter decrements smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.byKey(incrementKey));
+    await tester.pump();
+
+    expect(find.text('1'), findsOneWidget);
+    expect(find.text('0'), findsNothing);
+
+    await tester.tap(find.byKey(decrementKey));
+    await tester.pump();
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+  });
+
+  testWidgets('Counter decrements smoke test 2', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.byKey(incrementKey));
+    await tester.tap(find.byKey(incrementKey));
+    await tester.pump();
+
+    expect(find.text('2'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    await tester.tap(find.byKey(decrementKey));
+    await tester.pump();
+
+    expect(find.text('2'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
 }
